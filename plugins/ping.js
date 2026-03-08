86127a0c435cc4e0c95578562d9acff4295aeb59
Instagrem-com <aljokerxd111@gmail.com>
2026-03-08 04:42:11 +0200

    Update ping.js

--- plugins/ping.js
+++ plugins/ping.js
@@ -22,3 +22,19 @@ module.exports = {
     });
   }
 };
+
+
+module.exports = {
+  command: 'يوسف',
+  aliases: ['p', 'pong'],
+  description: 'Check bot response time',
+  usage: '.ping',
+  isPrefixless: true,
+  
+  async handler(sock, message, args) {
+    const start = Date.now();
+    const chatId = message.key.remoteJid;
+    
+    const sent = await sock.sendMessage(chatId, { 
+      text: 'اسم المطور لوسيفر يا علق' 
+    });
